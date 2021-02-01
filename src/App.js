65f3ff899127ae832dd01bf3c6ddfd8a3a825a39
Jorge Meza <jorge.meza.ext@sodexo.com>
2021-02-01 12:30:15 -0300

    mantenedor de tareas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import TodoList from './components/TodoList'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
+  const notifyOk = (text) => {
+    toast.success(text, {
+      position: 'top-right',
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='todo-app'>
+      <TodoList alertOk={notifyOk} />
+      <ToastContainer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
